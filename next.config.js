fbb6193070da44c69ede6365d5493e846e3d6c84
mihnea-eugen <mihnea@pospai.ro>
2026-05-12 00:19:12 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -8,6 +8,20 @@ const nextConfig = {
       { protocol: 'https', hostname: 'drdiana.ro' },
       { protocol: 'https', hostname: 'chirurgia-intima.com' }
     ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/proceduri/circumcizie-adulti',
+        destination: '/proceduri/circumcizie',
+        permanent: true
+      },
+      {
+        source: '/proceduri/vaginoplastie',
+        destination: '/proceduri',
+        permanent: true
+      }
+    ]
   }
 }
 module.exports = nextConfig
