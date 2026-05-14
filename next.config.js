e4e0f3a3517c9b56e849f36a537a10de84831ce9
mihnea-eugen <mihnea@pospai.ro>
2026-05-14 15:57:05 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -12,36 +12,13 @@ const nextConfig = {
   },
   async redirects() {
     return [
-      {
-        source: '/proceduri/circumcizie-adulti',
-        destination: '/proceduri/circumcizie',
-        permanent: true
-      },
-      {
-        source: '/proceduri/vaginoplastie',
-        destination: '/proceduri',
-        permanent: true
-      },
-      {
-        source: '/proceduri/alungire-penis',
-        destination: '/proceduri/faloplastie',
-        permanent: true
-      },
-      {
-        source: '/proceduri/ingrosare-lipofilling',
-        destination: '/proceduri/ingrosare-penis-lipofilling',
-        permanent: true
-      },
-      {
-        source: '/proceduri/ingrosare-acid-hialuronic',
-        destination: '/proceduri/marire-penis-acid-hialuronic',
-        permanent: true
-      },
-      {
-        source: '/chirurgie-intima',
-        destination: '/genitoplastie',
-        permanent: true
-      }
+      { source: '/proceduri/circumcizie-adulti', destination: '/proceduri/circumcizie', permanent: true },
+      { source: '/proceduri/vaginoplastie', destination: '/proceduri', permanent: true },
+      { source: '/proceduri/alungire-penis', destination: '/proceduri/faloplastie', permanent: true },
+      { source: '/proceduri/ingrosare-lipofilling', destination: '/proceduri/ingrosare-penis-lipofilling', permanent: true },
+      { source: '/proceduri/ingrosare-acid-hialuronic', destination: '/proceduri/marire-penis-acid-hialuronic', permanent: true },
+      { source: '/proceduri/perineorafie', destination: '/proceduri', permanent: true },
+      { source: '/chirurgie-intima', destination: '/genitoplastie', permanent: true }
     ]
   }
 }
